17b4077479b4a599773cff87aa9b8b664e91689a
mihai <udreamihai@gmail.com>
2022-02-08 16:05:41 +0000

    working hard

--- src/main/java/com/sparta/employeecsv/view/ReadFile.java
+++ src/main/java/com/sparta/employeecsv/view/ReadFile.java
@@ -1,17 +1,12 @@
 package com.sparta.employeecsv.view;
 
-import com.sparta.employeecsv.model.Employee;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -29,7 +24,7 @@ public class ReadFile {
             while ((line = br.readLine()) != null) { //read the file line by line
                 String[] data = line.split(","); //extract individual fields from each line
 
-                Employee employee = new Employee(Integer.valueOf(data[0]), data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]); //create new Employee object
+                Employee employee = new Employee(Integer.valueOf(data[0]), data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], Integer.valueOf(data[9])); //create new Employee object
 
                 employees.add(employee); //add the object to the List
             }
