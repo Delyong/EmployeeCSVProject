0b8deacb9c6fffcfd0b11eb55ede95fcfc3426bf
mihai <udreamihai@gmail.com>
2022-02-08 15:40:28 +0000

    working hard

--- src/main/java/com/sparta/employeecsv/model/Employee.java
+++ src/main/java/com/sparta/employeecsv/model/Employee.java
@@ -2,7 +2,7 @@ package com.sparta.employeecsv.model;
 
 public class Employee{
 
-    private String id;
+    private Integer id;
     private String namePrefix;
     private String firstName;
     private String middleInitial;
@@ -16,92 +16,21 @@ public class Employee{
     public Employee(){
     }
 
-    public Employee(String id, String namePrefix, String firstName, String middleInitial, String lastName, String gender, String email, String dob, String dateOfJoining, String salary){
+    public Employee(Integer id, String namePrefix, String firstName, String middleInitial, String lastName, String gender, String email, String dob, String dateOfJoining, String salary){
         this.id = id;
         this.namePrefix = namePrefix;
         this.firstName = firstName;
         this.lastname = lastName;
         this.middleInitial = middleInitial;
         this.gender = gender;
+        this.email = email;
+        this.dob = dob;
+        this.dateOfJoining = dob;
+        this.salary = salary;
     }
 
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNamePrefix() {
-        return namePrefix;
-    }
-
-    public void setNamePrefix(String namePrefix) {
-        this.namePrefix = namePrefix;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleInitial() {
-        return middleInitial;
-    }
-
-    public void setMiddleInitial(String middleInitial) {
-        this.middleInitial = middleInitial;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getDateOfJoining() {
-        return dateOfJoining;
-    }
-
-    public void setDateOfJoining(String dateOfJoining) {
-        this.dateOfJoining = dateOfJoining;
-    }
-
-    public String getSalary() {
-        return salary;
-    }
-
-    public void setSalary(String salary) {
-        this.salary = salary;
-    }
 }
--- src/main/java/com/sparta/employeecsv/view/ReadFile.java
+++ src/main/java/com/sparta/employeecsv/view/ReadFile.java
@@ -17,6 +17,8 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static java.util.Objects.isNull;
+
 public class ReadFile {
     String fileName = "EmployeeRecords.csv";
     public void readFile(String fileName){
@@ -27,7 +29,7 @@ public class ReadFile {
             while ((line = br.readLine()) != null) { //read the file line by line
                 String[] data = line.split(","); //extract individual fields from each line
 
-                Employee employee = new Employee(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]); //create new Employee object
+                Employee employee = new Employee(Integer.valueOf(data[0]), data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]); //create new Employee object
 
                 employees.add(employee); //add the object to the List
             }
@@ -36,8 +38,15 @@ public class ReadFile {
             e.printStackTrace();
         }
     }
+
     public static List<Employee> getDuplicates(final List<Employee> duplicatesList){
         return duplicatesList.stream().filter(duplicateByKey(Employee::getId))
                 .collect(Collectors.toList());
+    }
+
+    private static <T> Predicate<T> duplicateByKey(final Function<? super T, Object> keyExtractor) {
+        Map<Object,Boolean> seen = new ConcurrentHashMap<>();
+        return t -> isNull(seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE));
+
     }
 }
