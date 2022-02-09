aefac16c8d3193c640562cc29f3a08ba7e66080f
DalePBernardo <dalepb18@gmail.com>
2022-02-09 10:48:25 +0000

    Merged the employee class and test

--- src/main/java/com/sparta/employeecsv/model/Employee.java
+++ src/main/java/com/sparta/employeecsv/model/Employee.java
@@ -1,33 +1,31 @@
 package com.sparta.employeecsv.model;
 
 import java.sql.Date;
-import java.util.Arrays;
 
 public class Employee {
 
     private Integer employeeID;
-    
+
     private String namePrefix;
     private String firstName;
     private Character middleInitial;
     private String lastName;
-    
+
     private Character gender;
     private String email;
     private Date dateOfBirth;
     private Date dateOfJoin;
-    
+
     private Float salary;
-    
+
     public Employee(String employeeID, String namePrefix, String firstName,
                     String middleInitial, String lastName, String gender,
                     String email, String dateOfBirth, String dateOfJoin,
                     String salary
-    )
-    {
-    
-        this.employeeID = convertEmployeeId(employeeID);
-    
+    ) {
+
+        this.employeeID = parseEmployeeId(employeeID);
+
         this.firstName = parseName(firstName);
         this.middleInitial = parseMiddleInitial(middleInitial);
         this.lastName = parseName(lastName);
@@ -35,103 +33,105 @@ public class Employee {
 
         this.dateOfBirth = parseDate(dateOfBirth);
         this.dateOfJoin = parseDate(dateOfJoin);
-    
+
     }
-    
+
     public Employee() {
         super();
     }
-    
+
+    public Integer getEmployeeID() {
+        return employeeID;
+    }
+
+    public boolean isRecordValid() {
+
+        if (employeeID == null || namePrefix == null || firstName == null ||
+                middleInitial == null || lastName == null || gender == null ||
+                email == null || dateOfBirth == null || dateOfJoin == null ||
+                salary == null) {
+            return false;
+        }
+
+        return true;
+    }
+
     public String parseName(String name) {
         if (name == null || name.length() == 0) {
             return null;
         }
-    
+
         return name;
     }
-    
+
     public Character parseMiddleInitial(String middleInitial) {
-    
+
         if (middleInitial == null || middleInitial.length() != 1) {
             return null;
         }
-    
+
         return middleInitial.charAt(0);
     }
-    
+
     // require yyyy-mm-dd format
     // now mm-dd-yyyy
     public Date parseDate(String date) {
-    
-        if (date == null) { return null; }
-    
+
+        if (date == null) {
+            return null;
+        }
+
         // check length of date
         StringBuilder sb = new StringBuilder();
         String[] dateSplitArray = date.split("/");
-    
+
         // if the array doesn't have 3 elements an error will be thrown
         if (dateSplitArray.length != 3) {
             return null;
         }
-    
+
         sb.append(dateSplitArray[2]).append("-")
                 .append(dateSplitArray[0]).append("-")
                 .append(dateSplitArray[1]);
-    
+
         try {
             return Date.valueOf(sb.toString());
         } catch (IllegalArgumentException e) {
             return null;
         }
-    
+
     }
-    
-    public static Integer convertEmployeeId(String employeeID) {
+
+    public static Integer parseEmployeeId(String employeeID) {
         if (employeeID.matches("[0-9]{1,5}"))
             return Integer.valueOf(employeeID);
         else
             return null;
     }
-    
-    
-        public static String convertNamePrefix(String namePrefix){
-            if (namePrefix.matches("[a-zA-Z]{2,3}."))
-                return namePrefix;
-            else
-                return null;
-        }
 
-    public static Character convertGender(String gender){
+
+    public static String parseNamePrefix(String namePrefix) {
+        if (namePrefix.matches("[a-zA-Z]{2,3}."))
+            return namePrefix;
+        else
+            return null;
+    }
+
+    public static Character parseGender(String gender) {
         if (gender.matches("F|M"))
             return gender.charAt(0);
         else
             return null;
     }
-        public Integer getEmployeeID() {
-            return employeeID;
-        }
-```java
-public boolean isRecordValid() {
 
-    if (employeeID == null || namePrefix == null || firstName == null ||
-            middleInitial == null || lastName == null || gender == null ||
-            email == null || dateOfBirth == null || dateOfJoin == null ||
-            salary == null)
-    {
-        return false;
-    }
-
-    return true;
-```
-
-    public static String convertEmail(String email){
+    public static String parseEmail(String email) {
         if (email.matches("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$"))
             return email;
         else
             return null;
     }
-    
-    public static Float convertSalary(String salary){
+
+    public static Float parseSalary(String salary) {
         if (salary.matches("[0-9]{1,10}"))
             return Float.valueOf(salary);
         else
--- src/test/java/com/sparta/employeecsvtest/EmployeeTest.java
+++ src/test/java/com/sparta/employeecsvtest/EmployeeTest.java
@@ -1,11 +1,15 @@
 package com.sparta.employeecsvtest;
 
 import com.sparta.employeecsv.model.Employee;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,65 +19,132 @@ public class EmployeeTest {
 
     @BeforeAll
     public static void setUp(){
-        employee = new Employee("1", "Mrs.", "John", "A", "Smith", "F", "lavon.shufelt@aol.com", "12/19/1977","7/23/2000", "184597");
+        employee = new Employee();;
+    }
+
+    @DisplayName("Given date '12/19/1977', parseDate.toString returns a date string that " +
+            "matches the expected date")
+    @Test
+    public void givenAValidDate_parseDate_returnsMatchingDate() {
+        Date date = employee.parseDate("12/19/1977");
+        Assertions.assertEquals("1977-12-19", date.toString());
+    }
+
+    @DisplayName("Given an invalid date '122/6/1000', parseDate returns null")
+    @Test
+    public void givenAInvalidDate_parseDate_returnsNull() {
+        Date date = employee.parseDate("122/6/1000");
+        Assertions.assertEquals(null, date);
+    }
+
+    @DisplayName("Given a null date, parseDate returns null")
+    @Test
+    public void givenANullDate_parseDate_returnsNull() {
+        Date date = employee.parseDate(null);
+        Assertions.assertEquals(null, date);
+    }
+
+    @DisplayName("Given a null name, parseName returns null")
+    @Test
+    public void givenANullName_parseName_returnsNull() {
+        String name = employee.parseName(null);
+        Assertions.assertEquals(null, name);
+    }
+
+    @DisplayName("Given name 'Roberto', parseName matches name")
+    @Test
+    public void givenAValidName_parseName_matchesName() {
+        String name = employee.parseName("Roberto");
+        Assertions.assertEquals("Roberto", name);
+    }
+
+    @DisplayName("Given an empty name, parseName returns null")
+    @Test
+    public void givenAnEmptyName_parseName_returnsNull() {
+        String name = employee.parseName("");
+        Assertions.assertEquals(null, name);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"R,R", "X,X", "M,M"})
+    @DisplayName("Given a valid middleInitial, parseMiddleInitial returns expected character")
+    public void givenAValidMiddleInitial_parseMiddleInitial_matchingCharacter(
+            String middleInitial, Character expected)
+    {
+        Character actual = employee.parseMiddleInitial(middleInitial);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Given a middleInitial 'RM', parseMiddleInitial returns null")
+    @Test
+    public void givenA2LengthMiddleInitial_parseMiddleInitial_returnsNull() {
+        Character middleInitial = employee.parseMiddleInitial("RM");
+        Assertions.assertEquals(null, middleInitial);
+    }
+
+    @DisplayName("Given a null middle initial, parseMiddleInitial returns null")
+    @Test
+    public void givenANullMiddleInitial_parseMiddleInitial_returnsNull() {
+        Character middleInitial = employee.parseMiddleInitial(null);
+        Assertions.assertEquals(null, middleInitial);
     }
 
     @Test
     @DisplayName("Given a valid employee ID, convertEmployeeId, should return the integer value")
     public void givenAValidEmployeeId_ConvertEmployeeId_ReturnsTheIntegerValue(){
-        assertEquals(1240, employee.convertEmployeeId("1240"));
+        assertEquals(1240, employee.parseEmployeeId("1240"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"12f", "test", "!%", "34af*"})
     @DisplayName("Given a invalid employee ID, convertEmployeeId, should return null")
     public void givenAnInvalidEmployeeId_ConvertEmployeeId_ReturnsNull(String employeeId){
-        assertEquals(null, employee.convertEmployeeId(employeeId));
+        assertEquals(null, employee.parseEmployeeId(employeeId));
     }
 
     @Test
     @DisplayName("Given a valid name prefix, convertEmployeeId, should return the the string value with correct format")
     public void givenAValidNamePrefix_ConvertNamePrefix_ReturnsTheStringValue(){
-        assertEquals("Mr.", employee.convertNamePrefix("Mr."));
+        assertEquals("Mr.", employee.parseNamePrefix("Mr."));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Mrrss.", "Mr", "*%&.", "Mrs13."})
     @DisplayName("Given a invalid name prefix, convertNamePrefix, should return null")
     public void givenAnInvalidNamePrefix_ConvertNamePrefix_ReturnsNull(String namePrefix){
-        assertEquals(null, employee.convertNamePrefix(namePrefix));
+        assertEquals(null, employee.parseNamePrefix(namePrefix));
     }
 
     @Test
     @DisplayName("Given a valid gender, convertGender, should return the the character value")
     public void givenAValidGender_ConvertGender_ReturnsTheCharacterValue(){
-        assertEquals('F', employee.convertGender("F"));
+        assertEquals('F', employee.parseGender("F"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Male", "M12", "%$^", "34adf*"})
     @DisplayName("Given a invalid gender, convertGender, should return null")
     public void givenAnInvalidGender_ConvertGender_ReturnsNull(String gender){
-        assertEquals(null, employee.convertGender(gender));
+        assertEquals(null, employee.parseGender(gender));
     }
 
     @Test
     @DisplayName("Given a valid email, convertEmail, should return the string valid email format")
     public void givenAValidEmail_ConvertEmail_ReturnsTheStringValue(){
-        assertEquals("test@gmail.com", employee.convertEmail("test@gmail.com"));
+        assertEquals("test@gmail.com", employee.parseEmail("test@gmail.com"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"notanemail", "test$%£@gmail.com", "1234asdf£", "testgmailcom"})
     @DisplayName("Given a invalid email, convertEmail, should return null")
     public void givenAnInvalidEmail_ConvertEmail_ReturnsNull(String email){
-        assertEquals(null, employee.convertEmail(email));
+        assertEquals(null, employee.parseEmail(email));
     }
 
     @Test
     @DisplayName("Given a valid salary, convertSalary, should return the integer value")
     public void givenAValidSalary_ConvertSalary_ReturnsTheIntegerValue(){
-        assertEquals(45000, employee.convertSalary("45000"));
+        assertEquals(45000, employee.parseSalary("45000"));
     }
 
 
@@ -81,6 +152,6 @@ public class EmployeeTest {
     @ValueSource(strings = {"12bghjue", "%$53", " ", "notanumber"})
     @DisplayName("Given a invalid employee ID, convertEmployeeId, should return null")
     public void givenAnInvalidSalary_ConvertSalary_ReturnsNull(String salary){
-        assertEquals(null, employee.convertSalary(salary));
+        assertEquals(null, employee.parseSalary(salary));
     }
 }
